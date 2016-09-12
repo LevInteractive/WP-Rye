c8bd2a6a252382de8adf87a76af583184e39f5e8
Pete Saia <pete@petesaia.com>
2016-09-12 12:34:42 -0400

    Added rye translation text

--- templates/header.php
+++ templates/header.php
@@ -7,9 +7,10 @@
       <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
+
     <!--[if IE]>
       <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'rye'); ?>
       </div>
     <![endif]-->
 
